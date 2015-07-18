cb9b7e7c16122332ace9f5a261c0e6b117e1c629
Bala Clark <bala.clark@clock.co.uk>
2015-07-18 17:59:21 +0200

    use arrow functions

--- test/view/load-indicator.test.js
+++ test/view/load-indicator.test.js
@@ -1,29 +1,29 @@
 let assert = require('assert')
 let LoadIndicator = require('../../app/view/load-indicator')
 
-describe('LoadIndicator', function () {
+describe('LoadIndicator', () => {
 
-  it('should render on init', function () {
+  it('should render on init', () => {
     let indicator = new LoadIndicator()
     assert.equal(indicator.el.outerHTML
       , '<div id="cb-loading-overlay" style="display: none; "></div>')
   })
 
-  it('should show', function () {
+  it('should show', () => {
     let indicator = new LoadIndicator()
     assert.equal(indicator.el.style.display, 'none')
     indicator.show()
     assert.equal(indicator.el.style.display, 'block')
   })
 
-  it('should hide', function () {
+  it('should hide', () => {
     let indicator = new LoadIndicator()
     indicator.show()
     indicator.hide()
     assert.equal(indicator.el.style.display, 'none')
   })
 
-  it('should emit a "show" event', function (done) {
+  it('should emit a "show" event', done => {
     let indicator = new LoadIndicator()
     indicator.on('show', function (e) {
       assert.deepEqual(indicator, e)
@@ -32,7 +32,7 @@ describe('LoadIndicator', function () {
     indicator.show()
   })
 
-  it('should emit a "hide" event', function (done) {
+  it('should emit a "hide" event', done => {
     let indicator = new LoadIndicator()
     indicator.on('hide', function (e) {
       assert.deepEqual(indicator, e)
